2f5b8fc4065439baaeb47da94768fa0d9b60c6c6
sergey_iagfarov <dedaz27@mail.ru>
2022-09-21 14:25:26 +0500

    1 задача

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        var friend =19;
+        System.out.println(friend);
+        friend = friend * 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+    }
+}
